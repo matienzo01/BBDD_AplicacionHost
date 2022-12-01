9ece46d8659fd07332a5fe04de7a0f33fb68f2eb
Matias <matiasgutierrez225@gmail.com>
2022-11-30 22:44:44 -0300

    Corregida la conexion a la bbdd

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
 /**
  * Hello world!
  *
@@ -10,14 +17,30 @@ public class App
 
     public static void main( String[] args )
     {
-        String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=Club;integratedSecurity=true";
-
-        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
-            // Code here.
+        
+        try{
+        	SQLServerDataSource ds = new SQLServerDataSource(); 
+        	
+        	ds.setServerName("DESKTOP-ON7JO5N");  
+        	//ds.setPortNumber(1433);
+        	ds.setDatabaseName("Club");  
+        	ds.setUser("sa");
+        	ds.setPassword("123456");
+        	ds.setTrustServerCertificate(true);
+        	
+        	Connection con = ds.getConnection(); 
+        	Statement sql = con.createStatement();
+        	ResultSet result = sql.executeQuery("SELECT * from Socio");
+        	
+        	while(result.next()) {
+        		System.out.println(result);
+        	}
+        	
+            System.out.println("Se conectp");
         }
         // Handle any errors that may have occurred.
         catch (SQLException e) {
             e.printStackTrace();
-        }
+        } 
     }
 }
